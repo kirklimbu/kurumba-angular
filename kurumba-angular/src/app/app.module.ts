45504ac27ff7499985ded5dfce2c2a138022d73e
bickram <kirklimbu@gmail.com>
2020-05-24 14:20:47 +0545

    deployment configuration setup via now.sh

--- kurumba-angular/src/app/app.module.ts
+++ kurumba-angular/src/app/app.module.ts
@@ -1,41 +1,12 @@
-import { ToastrService } from 'ngx-toastr';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HttpClientModule } from '@angular/common/http';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-// import { NpDatepickerModule } from 'angular-nepali-datepicker';
-
-
 
 @NgModule({
-  declarations: [
-    AppComponent,
-
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    NgbModule,
-    HttpClientModule,
-    FontAwesomeModule,
-    // NpDatepickerModule
-
-
-  ],
-  providers: [
-    {provide: ToastrService, useClass: ToastrService,}
-
-  ],
-  schemas: [
-    CUSTOM_ELEMENTS_SCHEMA,
-    NO_ERRORS_SCHEMA
-  ],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- kurumba-angular/src/environments/environment.prod.ts
+++ kurumba-angular/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8082/api',
+  apiUrl: "http://localhost/api",
 
 };
