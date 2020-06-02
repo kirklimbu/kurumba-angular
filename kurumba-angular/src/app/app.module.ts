a91ff3232bfc61a5dc76bc408eceb362ac5a78d8
bickram <kirklimbu@gmail.com>
2020-06-02 12:06:53 +0545

    merge conflict resolved

--- kurumba-angular/src/app/app.module.ts
+++ kurumba-angular/src/app/app.module.ts
@@ -1,12 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+     FontAwesomeModule,
+     NgbModule,
+     AppRoutingModule,
+    //  FormsModule,
+     HttpClientModule
+
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
