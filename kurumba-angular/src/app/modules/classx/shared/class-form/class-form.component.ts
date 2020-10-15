8e9527158fe00a78f508124cf58359f00878b9fa
bickram sambahamphe <kirklimbu@gmail.com>
2020-10-15 23:51:03 +0545

    teacher-list , subjectList bug fix

--- kurumba-angular/src/app/modules/classx/shared/class-form/class-form.component.ts
+++ kurumba-angular/src/app/modules/classx/shared/class-form/class-form.component.ts
@@ -1,19 +1,19 @@
-import { ActivatedRoute, Router } from "@angular/router";
-import { Component, OnInit } from "@angular/core";
-import { FormArray, FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-import { Classes } from "src/app/shared/models/classes.model";
-import { ClassxService } from "../../services/classx.service";
-import { NgxSpinnerService } from "ngx-spinner";
-import { OntypeValidationService } from "ontype-validations";
-import { Subject } from "src/app/shared/models/subject.model";
-import { finalize } from "rxjs/operators";
-import { ToastrService } from "ngx-toastr";
+import { Classes } from 'src/app/shared/models/classes.model';
+import { ClassxService } from '../../services/classx.service';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { OntypeValidationService } from 'ontype-validations';
+import { Subject } from 'src/app/shared/models/subject.model';
+import { finalize } from 'rxjs/operators';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
-  selector: "app-class-form",
-  templateUrl: "./class-form.component.html",
-  styleUrls: ["./class-form.component.scss"],
+  selector: 'app-class-form',
+  templateUrl: './class-form.component.html',
+  styleUrls: ['./class-form.component.scss'],
 })
 export class ClassFormComponent implements OnInit {
   class: Classes = new Classes();
@@ -21,8 +21,8 @@ export class ClassFormComponent implements OnInit {
   subject: Subject = new Subject();
   classForm: FormGroup;
   formSubmitted: true;
-  getErrorMessage = "";
-  mode = "";
+  getErrorMessage = '';
+  mode = '';
 
   constructor(
     private router: Router,
@@ -42,12 +42,12 @@ export class ClassFormComponent implements OnInit {
   getParamsFromUrl() {
     this.route.params.subscribe((params) => {
       const classId = params.id;
-      console.log("param class id " + classId);
+      console.log('param class id ' + classId);
 
       if (classId != null && classId) {
-        console.log("class id " + classId);
+        console.log('class id ' + classId);
 
-        this.mode = "edit";
+        this.mode = 'edit';
         this.spinner.show();
         /* replace this code with getClassById() */
         this.classxService
@@ -55,15 +55,15 @@ export class ClassFormComponent implements OnInit {
           .pipe(finalize(() => this.spinner.hide()))
 
           .subscribe((res) => {
-            console.log("edit class data" + JSON.stringify(res));
+            console.log('edit class data' + JSON.stringify(res));
 
             this.class = res.filter((f) => f.classId == classId);
-            console.log("filtered class " + JSON.stringify(this.class));
+            console.log('filtered class ' + JSON.stringify(this.class));
 
             this.buildForm();
           }),
           (err) => {
-            this.toastr.error("Error fetching classes");
+            this.toastr.error('Error fetching classes');
           };
       } else {
         this.buildForm();
@@ -72,10 +72,10 @@ export class ClassFormComponent implements OnInit {
   }
 
   buildForm() {
-    console.log("build form called");
+    console.log('build form called');
 
-    if (this.mode === "add") {
-      console.log("inside class add");
+    if (this.mode === 'add') {
+      console.log('inside class add');
 
       this.classForm = this.fb.group({
         classId: this.class.classId,
@@ -83,7 +83,7 @@ export class ClassFormComponent implements OnInit {
         subjectCollection: this.fb.array([this.buildSubjectForm()]),
       });
     } else {
-      console.log("inside class edit");
+      console.log(this.class);
 
       this.classForm = this.fb.group({
         classId: this.class.classId,
@@ -98,11 +98,11 @@ export class ClassFormComponent implements OnInit {
   }
 
   subjectCollection(): FormArray {
-    return this.classForm.get("subjectCollection") as FormArray;
+    return this.classForm.get('subjectCollection') as FormArray;
   }
 
   buildSubjectForm() {
-    console.log("subject build form called");
+    console.log('subject build form called');
 
     return this.fb.group({
       subjectId: this.subject.subjectId,
@@ -127,7 +127,7 @@ export class ClassFormComponent implements OnInit {
   onSave() {
     this.formSubmitted = true;
 
-    console.log("class form values " + JSON.stringify(this.classForm.value));
+    console.log('class form values ' + JSON.stringify(this.classForm.value));
     if (this.classForm.valid && this.formSubmitted) {
       this.spinner.show();
       this.classxService
@@ -135,22 +135,22 @@ export class ClassFormComponent implements OnInit {
         .pipe(finalize(() => this.spinner.hide()))
         .subscribe(
           (res) => {
-            this.toastr.success("Class SuccessFully Added.");
-            this.router.navigate(["../"], { relativeTo: this.route });
+            this.toastr.success('Class SuccessFully Added.');
+            this.router.navigate(['../'], { relativeTo: this.route });
           },
           (err) => {
-            console.log("save server error" + JSON.stringify(err));
+            console.log('save server error' + JSON.stringify(err));
 
             err = err.error.message
               ? this.toastr.error(err.error.message)
-              : this.toastr.error("Error adding new class.");
+              : this.toastr.error('Error adding new class.');
           }
         );
     }
   }
 
   onCancel() {
-    const link: any = this.mode === "add" ? "../" : "../../";
+    const link: any = this.mode === 'add' ? '../' : '../../';
     this.router.navigate([link], { relativeTo: this.route });
   }
 
